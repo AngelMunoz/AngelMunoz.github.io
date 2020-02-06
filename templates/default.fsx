286aa2bf7906c411da78e588d932cb4ac26ca737
Angel D. Munoz <scythes.lol@gmail.com>
2020-02-06 13:34:50 -0700

    update website

--- templates/default.fsx
+++ templates/default.fsx
@@ -7,6 +7,9 @@ let defaultPage (siteModel: SiteModel) pageTitle content =
     html []
         [ head []
               [ meta [ CharSet "utf-8" ]
+                meta
+                    [ Name "viewport"
+                      Content "width=device-width, initial-scale=1.0" ]
                 title [] [ (!!pageTitle) ]
                 link
                     [ Rel "stylesheet"
@@ -20,13 +23,24 @@ let defaultPage (siteModel: SiteModel) pageTitle content =
                     [ Rel "alternate"
                       Type "application/atom+xml"
                       Href "/feed.xml"
-                      HtmlProperties.Title "News Feed" ] ]
+                      HtmlProperties.Title "News Feed" ] 
+                script 
+                  [ Src "/scripts/index.js" ] [] ]
           body []
               [ main [ Class "hero is-fullheight is-dark is-bold" ]
                     [ div [ Class "hero-head" ]
                           [ header [ Class "navbar" ]
                                 [ div [ Class "container" ]
-                                      [ div [ Class "navbar-menu" ]
+                                      [ div [ Class "navbar-brand" ]
+                                            [ div
+                                                [ Class "navbar-burger"
+                                                  Id "burger" ]
+                                                  [ span [ ] []
+                                                    span [ ] []
+                                                    span [ ] [] ] ]
+                                        div
+                                            [ Class "navbar-menu"
+                                              Id "menusin" ]
                                             [ div [ Class "navbar-end" ]
                                                   [ a
                                                       [ Class "navbar-item"
@@ -34,15 +48,28 @@ let defaultPage (siteModel: SiteModel) pageTitle content =
                                                     a
                                                         [ Class "navbar-item"
                                                           Href "/archive.html" ] [ !!"Archive" ] ] ] ] ] ]
-                      div [ Class "hero-body container is-column" ]
+                      div [ Class "hero-body container is-column is-fluid" ]
                           [ yield h1 [ Class "title" ] [ !!siteModel.title ]
                             for cont in content do
                                 yield cont ]
                       div [ Class "hero-foot" ]
-                          [ nav [ Class "tabs is-toggle is-toggle-rounded is-fullwidth is-large" ]
+                          [ nav [ Class "tabs is-toggle is-toggle-rounded is-fullwidth is-small" ]
                                 [ div [ Class "container" ]
                                       [ ul []
-                                            [ li [] [ a [ Target "_blank" ; Href "https://github.com/AngelMunoz/"] [ !!"Github" ] ]
-                                              li [] [ a [ Target "_blank" ; Href "https://gitlab.com/AngelMunoz/"] [ !!"Gitlab" ] ]
-                                              li [] [ a [ Target "_blank" ; Href "https://twitter.com/daniel_tuna/"] [ !!"Twitter" ] ]
-                                              li [] [ a [ Target "_blank" ; Href "https://www.linkedin.com/in/angeldmunoz/"] [ !!"Linked In" ] ] ] ] ] ] ] ] ]
+                                            [ li []
+                                                  [ a
+                                                      [ Target "_blank"
+                                                        Href "https://github.com/AngelMunoz/" ] [ !!"Github" ] ]
+                                              li []
+                                                  [ a
+                                                      [ Target "_blank"
+                                                        Href "https://gitlab.com/AngelMunoz/" ] [ !!"Gitlab" ] ]
+                                              li []
+                                                  [ a
+                                                      [ Target "_blank"
+                                                        Href "https://twitter.com/daniel_tuna/" ] [ !!"Twitter" ] ]
+                                              li []
+                                                  [ a
+                                                      [ Target "_blank"
+                                                        Href "https://www.linkedin.com/in/angeldmunoz/" ]
+                                                        [ !!"Linked In" ] ] ] ] ] ] ] ] ]
--- templates/post.fsx
+++ templates/post.fsx
@@ -1,6 +1,7 @@
 #load "../siteModel.fsx"
 #load "default.fsx"
 
+open System
 open Html
 open SiteModel
 
@@ -16,16 +17,17 @@ type Model =
 let generate (siteModel: SiteModel) (mdl: Model) (posts: Post list) (content: string) =
     let post =
         article [ Class "post" ]
-            [ h1 [ Class "post-title" ] [ !!mdl.title ]
-              div [ Class "post-date" ] [ !!mdl.published.ToShortDateString() ]
+            [ h1 [ Class "title" ] [ !!mdl.title ]
+              div [ Class "is-size-5" ]
+                  [ yield !!mdl.published.ToShortDateString()
+                    if mdl.published.Subtract(TimeSpan.FromDays(365.0)).Year < DateTime.Today.Year then
+                        yield h3 [ Class "has-text-warning is-size-4" ]
+                                  [ !!"Notice! This post seems to be at least 1 year old, the contents might not be accurate anymore." ] ]
               div []
                   [ ul [ Class "tags" ]
                         [ for tag in mdl.tags do
                             yield li [ Class "tag is-light" ] [ a [] [ !!tag ] ] ] ]
-              section [ Class "section"] [!!content]
-              section [Id "disqus_thread"] [
-                script [ Src "/scripts/disqus.js"] []
-              ]
-            ]
+              section [ Class "content" ] [ !!content ]
+              section [ Id "disqus_thread" ] [ script [ Src "/scripts/disqus.js" ] [] ] ]
 
     Default.defaultPage siteModel mdl.title [ post ]
